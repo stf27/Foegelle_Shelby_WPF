59915007f58f3e2bce4619560e2f86f42c742afd
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-11 17:06:23 -0500

    Expressions Personal 03
    Notes and comments on how to go about calculating expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,3 +4,24 @@
  */
 
 // How many creeps you need to farm to buy an item. While taking into account the amount of passive gold you have made.
+
+// Does not take into account gold generating items simply because there is no telling what time the player will no longer have that item.
+
+// Does not take into account gold generating masteries
+// This is due to unpredictable factors.
+
+// Time in game
+
+// % creeps farmed
+
+// Value of each minion:
+// Melee: 19g + (0.5g * (gameTime / 180sec))
+// Caster: 14g + (0.2g * (gameTime / 90sec))
+// Siege: 40g + (1g * (gameTime / 180sec))
+
+// Value of each wave (in sets of three):
+// First two waves same value:
+// Last wave higher value: 
+
+// Passive Gold:
+// 19g / 10sec
