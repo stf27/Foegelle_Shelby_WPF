2b1771462a5b4bdb9e05d55a14d3e9c5cc387924
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-11 17:21:57 -0500

    Expressions Personal 04
    set up variables

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,11 +3,11 @@
  * Expressions - Personal
  */
 
-// How many creeps you need to farm to buy an item. While taking into account the amount of passive gold you have made.
+// How many minions do you need to farm to buy an item? While taking into account the amount of passive gold you have made.
 
-// Does not take into account gold generating items simply because there is no telling what time the player will no longer have that item.
-
-// Does not take into account gold generating masteries
+// *Does not take into account gold generating items*
+// This is because there is no telling what time the player will no longer have that item.
+// *Does not take into account the gold generating masteries Scavenger and Bandit*
 // This is due to unpredictable factors.
 
 // Time in game
@@ -20,8 +20,25 @@
 // Siege: 40g + (1g * (gameTime / 180sec))
 
 // Value of each wave (in sets of three):
-// First two waves same value:
-// Last wave higher value: 
+// First two waves same value: meleeValue * 3 + casterValue * 3
+// Last wave higher value: meleeValue * 3 + casterValue * 3 + siegeValue
 
 // Passive Gold:
 // 19g / 10sec
+
+// gameTime in terms of minutes
+var gameTime = 5;
+
+// percentMinionsFarmed in terms of percent not in decimal form
+var percentMinionsFarmed = 100;
+
+// greedLevel refers to the mastery called Greed. Can be a value of 0, 1, 2, 3.
+// Level 1 Greed = 0.5g / 10sec
+// Level 2 Greed = 1.0g / 10sec
+// Level 3 Greed = 1.5g / 10sec
+var greedLevel = 2;
+
+// wealth refers to the mastery called Wealth.
+// If the player has Wealth activated, then they start the game with an extra 40g.
+// Correct inputs for wealth include yes, no.
+var wealth = "no";
