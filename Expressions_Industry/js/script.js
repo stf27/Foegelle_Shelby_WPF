64cfd63b6217883b3d9070b09465c7462b61d1ae
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-11 20:12:08 -0500

    Industry 03
    testing formulas with output

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,9 +3,9 @@
  * Expressions - Industry
  */
 
-// This calculator calculates how much you should charge a client hourly for freelance web design based on overhead, hours worked weekly, and the salary the user wishes to make.
+// This calculator calculates how much you should charge a client hourly for freelance web design based on salary the user wishes to make,
+// hours worked weekly, and overhead (for things like an office, computers, etc.).
 
-    
 // Gets input value for weekly hours worked
 var targetSalary = prompt("How much do you want to make in a year?");
 // Checks if user input was the correct type. If not, alert user and ask for input again.
@@ -23,3 +23,13 @@ while (isNaN(hoursWorkedWeekly)) {
 }
 
 
+// Take target salary
+// Now factor in taxes, insuance, Social Security, etc.
+// A safe figure for the total cost of all these things is about 30%.
+// So take target salary and multiply that by 30% to find the costs
+var taxCosts = targetSalary * 0.3;
+
+targetSalary += taxCosts;
+
+console.log(taxCosts + " tax costs.")
+console.log(targetSalary + " total salary.");
