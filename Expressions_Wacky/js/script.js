c5a3daeb73ec5343a75faad4e4921f38ae0ba274
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-11 22:01:48 -0500

    Wacky 01
    Added inputs and an array data type

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,6 +3,27 @@
  * Expressions Wacky
  */
 
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", 3];
+// The calculator determines the percent chance you have to reach the end of a rainbow
+// depending on the amount of underwear you buy in a year, and several other factors...
 
-console.log(muppetNames)
+
+// Gets input value for type of underwear the user wears most frequently
+var underwearType = prompt("What type of underwear do you wear most often?\nAcceptable inputs are boxers, boxer briefs, or tighty-whiteys");
+// Checks if user input was the correct type. If not, alert user and ask for input again.
+while ((underwearType != "boxers") && (underwearType != "boxer briefs") && (underwearType != "tighty-whiteys")) {
+    alert("Illegal Input Type!");
+    underwearType = prompt("What type of underwear do you wear most often?\nAcceptable inputs are boxers, boxer briefs, or tighty-whiteys");
+}
+
+// Gets input value for total pairs of underwear purchased in a year
+var totalUnderwearPurchased = prompt("How many pairs of underwear do you purchase in a year?");
+// Checks if user input was the correct type. If not, alert user and ask for input again.
+while (isNaN(totalUnderwearPurchased)) {
+    alert("Illegal Input Type!");
+    totalUnderwearPurchased = prompt("How many pairs of underwear do you purchase in a year?");
+}
+
+var underwearInfo = [underwearType, totalUnderwearPurchased];
+
+console.log(underwearInfo[0] + " type");
+console.log(underwearInfo[1] + " total purchased");
