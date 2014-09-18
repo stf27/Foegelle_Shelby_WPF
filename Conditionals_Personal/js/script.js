f6aa500c0cdeb2be3932fa592a0f3e176009ef80
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-18 12:06:33 -0500

    Conditionals 02 - Personal finished first part of input

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,8 +8,22 @@
 
 // The wattage a device uses.
 var deviceWattageInput = prompt("Find the wattage of each device you want to calculate the costs to run it in a month.\nPlease enter each value into the box separated by a space.");
+// Removes all unwanted characters from the array and converts each element in the array into a number.
+var deviceWattageArray = deviceWattageInput.split(' ').map(Number);
 
-// Removes all unwanted characters from the array.
-var deviceWattageArray = deviceWattageInput.split(' ');
+// Checks if each element of deviceWattageArray is a number. If not, then ask for input again.
+var arrayIncrement;
+for(arrayIncrement = 0; arrayIncrement < deviceWattageArray.length; ++arrayIncrement) {
+    if(isNaN(deviceWattageArray[arrayIncrement])) {
+        alert("Incorrect Input!");
+        // Clears the array.
+        deviceWattageArray.length = 0;
+        deviceWattageInput = prompt("Find the wattage of each device you want to calculate the costs to run it in a month.\nPLEASE ENTER EACH VALUE INTO THE BOX SEPARATED BY A SPACE.");
+        deviceWattageArray = deviceWattageInput.split(' ').map(Number);
+    }
+}
+
+
 
 console.log("Array: " + deviceWattageArray[0]);
+console.log(typeof deviceWattageArray[0]);
