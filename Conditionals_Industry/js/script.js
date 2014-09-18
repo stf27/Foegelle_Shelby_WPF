8118a914bd8ee2d42c8eef95dfcb199b311c2f28
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-18 13:21:16 -0500

    Conditionals 07 - Industry decided on calculation

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,4 @@
  * Conditionals - Industry
  */
 
+// This calculates the font size you want to use in ems based off of the target value (size you want in pixels) and the context value (the font size of its container).
