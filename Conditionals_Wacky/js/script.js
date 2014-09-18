2c03af1df7733c741e23ada001d1146fc2a31345
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-18 15:02:53 -0500

    Conditionals 16 - Wacky determined idea for calculations

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,3 +4,6 @@
  * Conditionals - Wacky
  */
 
+// Calculates the percent chance a person has to find out how many licks it really does take to get to the center of a
+// tootsie roll pop based off of the frequency the user eats sweets, their age, and their own level of self control.
+
