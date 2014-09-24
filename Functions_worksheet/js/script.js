d61f115f34970f8d24848654fcc8f4cceaa16f81
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-24 11:54:18 -0500

    Functions WS 14 - code cleanup and comments

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -20,14 +20,14 @@
     // Outputs circumference value to console
     console.log("The circumference of the circle is " + circumference);
 
-// Stung
-
-// Variable that stores the weight of the animal
-var victimWeight = 0;
+// Stung!
 
 // Given
 // Number bee stings per pound to kill an animal
 var beeStingNum = 8.666666667;
+
+// Variable that stores the weight of the animal
+var victimWeight = 0;
 
 // Calculates the total number of stings it takes to kill an animal of a given weight
 function stingsToKill (weight) {
