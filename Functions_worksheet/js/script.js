8da9b40eecf7c33abb43b62147b9e1831beb3836
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-24 11:49:07 -0500

    Functions WS 11 - added var for stung

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,17 +6,21 @@
 
 // Circumference
 
-// Gets a radius value from the user
-var userRadiusInput = prompt("This program calculates the circumference of a circle.\nPlease enter a radius value.");
+    // Gets a radius value from the user
+    var userRadiusInput = prompt("This program calculates the circumference of a circle.\nPlease enter a radius value.");
 
-// Function that calculates the circumference of a circle
-function calculateCircumference(radius) {
-    return 2 * Math.PI * radius;
-}
+    // Function that calculates the circumference of a circle
+    function calculateCircumference(radius) {
+        return 2 * Math.PI * radius;
+    }
 
-// Stores circumference value here and sets decimal places to 2
-var circumference = calculateCircumference(userRadiusInput).toFixed(2);
+    // Stores circumference value here and sets decimal places to 2
+    var circumference = calculateCircumference(userRadiusInput).toFixed(2);
 
-// Outputs circumference value to console
-console.log("The circumference of the circle is " + circumference);
+    // Outputs circumference value to console
+    console.log("The circumference of the circle is " + circumference);
 
+// Stung
+
+// Variable that stores the weight of the animal
+var victimWeight = 0;
