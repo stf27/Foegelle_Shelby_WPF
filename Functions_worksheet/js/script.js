b0f21480d0d19894fe118c67a2ba2a49789da552
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-24 11:38:13 -0500

    Functions WS 05 - stored circ result in var

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -9,7 +9,10 @@
 // Radius of circle variable
 var circleRadius = 0;
 
+// Function that calculates the circumference of a circle
 function calculateCircumference(radius) {
     return 2 * Math.PI * radius;
 }
+
+var circumference = calculateCircumference(circleRadius);
 
