9dfc3c2ea4f709b5d76b73262842c0b166e2ed8a
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-24 17:41:14 -0500

    Functions Personal 03 - added form inputs

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,26 +5,57 @@
  */
 
 // This program helps you to calculate your monthly expenses and compares that to your monthly income.
+function myFunction(form) {
+
+    var firstName = form.fname.value;
 
 // Yearly Gross Income
+var yearlyGrossIncome = prompt("What is your yearly gross income?");
+// Checks if user input was the correct type. If not, alert user and ask for input again.
+while (isNaN(yearlyGrossIncome)) {
+alert("Illegal Input Type!");
+yearlyGrossIncome = prompt("What is your yearly gross income?\nNumbers only please!");
+}
+
 // Yearly Income Tax Rate
 
+
 // Rent/Mortgage
+
 // Education/Tuition
+
 // Credit Card Minimum Payments
+
 // Car Insurance
+
 // Gasoline
+
 // Other Car Expenses
+
 // Other Loan Payments
+
 // Home Insurance
+
 // Life Insurance
+
 // Health/Dental Insurance
+
 // Telephone
+
 // Cable/TV/Internet
+
 // Electricity
+
 // Water/Garbage
+
 // Groceries
+
 // Personal Care (clothing, haircuts, etc.)
+
 // Eating Out
+
 // Subscriptions
 
+    console.log("First Name: " + firstName);
+    //alert("You typed: " + firstName);
+}
