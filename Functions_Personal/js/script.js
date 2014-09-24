9e870b0796fbcaa528b1d72d3511d80f2f35d852
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-24 18:36:00 -0500

    Functions Personal 07 - stored variables in arrays

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,6 +7,12 @@
 // This program helps you to calculate your monthly expenses and compares that to your monthly income.
 function myFunction(form) {
 
+    // Variables get their value from the HTML form input boxes
+    // Array that stores the user's income and income tax amount
+    var income =[];
+    // Array that stores the user's expenses
+    var expenses = [];
+    /**
     // Yearly Gross Income
     var yearlyGrossIncome = form.yearGrossIncome.value;
     // Yearly Income Tax Rate
@@ -47,6 +53,54 @@ function myFunction(form) {
     var eatingOut = form.eatingOut.value;
     // Subscriptions
     var subscriptions = form.subscriptions.value;
+    **/
+
+    // Yearly Gross Income
+    income[0] = form.yearGrossIncome.value;
+    // Yearly Income Tax Rate
+    income[1] = form.yearIncomeTaxRate.value;
+    // Rent/Mortgage
+    expenses[0] = form.rentMortgage.value;
+    // Education/Tuition
+    expenses[1] = form.educationTuition.value;
+    // Credit Card Minimum Payments
+    expenses[2] = form.creditCardPayments.value;
+    // Car Insurance
+    expenses[3] = form.carInsurance.value;
+    // Gasoline
+    expenses[4] = form.gasoline.value;
+    // Other Car Expenses
+    expenses[5] = form.otherCarExpenses.value;
+    // Other Loan Payments
+    expenses[6] = form.otherLoanPayments.value;
+    // Home Insurance
+    expenses[7] = form.homeInsurance.value;
+    // Life Insurance
+    expenses[8] = form.lifeInsurance.value;
+    // Health/Dental Insurance
+    expenses[9] = form.healthDentalInsurance.value;
+    // Telephone
+    expenses[10] = form.telephone.value;
+    // TV/Internet
+    expenses[11] = form.tvInternet.value;
+    // Electricity
+    expenses[12] = form.electricity.value;
+    // Water/Garbage
+    expenses[13] = form.waterGarbage.value;
+    // Groceries
+    expenses[14] = form.groceries.value;
+    // Personal Care (clothing, haircuts, etc.)
+    expenses[15] = form.personalCare.value;
+    // Eating Out
+    expenses[16] = form.eatingOut.value;
+    // Subscriptions
+    expenses[17] = form.subscriptions.value;
+
+    function sum() {
+
+    }
+
+
 
     console.log("Yearly Income: " + yearlyGrossIncome);
     console.log("Yearly Income: " + yearlyIncomeTaxRate);
