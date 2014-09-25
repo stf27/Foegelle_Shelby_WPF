eb4b5b12aeff7e341d61865e480726f68f700bc7
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-25 11:28:03 -0500

    Functions Wacky 19 -  more functions and vars

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -22,10 +22,22 @@ while(isNaN(priceOfPizza)) {
     priceOfPizza = prompt("What is the price of the pizza?\nNumbers only please!");
 }
 
+// Calculates the area of a circle or more importantly the area of a pizza :)
 function areaOfCircle(diameter) {
     var radius = diameter / 2;
-    return Math.PI * (r * r);
+    return Math.PI * (radius * radius);
 }
+
+// Calculates the price of the pizza per square inch
+function pricePerSquareInch(area, price) {
+    return price / area;
+}
+
+// Stores the area of the pizza
+var areaOfPizza = areaOfCircle(diameterOfPizza);
+
+// Stores the price of the pizza
+var priceOfPizzaPerSquareInch = pricePerSquareInch(areaOfPizza, priceOfPizza);
 
 
 console.log("The diameter of the pizza is " + diameterOfPizza);
