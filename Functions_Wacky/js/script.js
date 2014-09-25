9213a4e08c272726e76ebe34807044e2247f36b8
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-25 11:16:28 -0500

    Functions Wacky 17 - added input

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,3 +4,23 @@
  * Functions - Wacky
  */
 
+// Pizza Value Evaluator
+// Takes the size of a pizza and the cost and tells the user the cost per square inch of pizza
+// Assumes the user is buying a whole round pizza
+
+// Gets the diameter of the pizza from the user and checks that it is the correct input type
+var diameterOfPizza = prompt("What is the diameter of the pizza (in inches)?");
+while(isNaN(diameterOfPizza)) {
+    alert("Incorrect Input!");
+    diameterOfPizza = prompt("What is the diameter of the pizza (in inches)?\nNumbers only please!");
+}
+
+// Gets the price of the pizza from the user and checks that it is the correct input type
+var priceOfPizza = prompt("What is the price of the pizza?");
+while(isNaN(priceOfPizza)) {
+    alert("Incorrect Input!");
+    priceOfPizza = prompt("What is the price of the pizza?\nNumbers only please!");
+}
+
+console.log("The diameter of the pizza is " + diameterOfPizza);
+console.log("The price of the pizza is " + priceOfPizza);
