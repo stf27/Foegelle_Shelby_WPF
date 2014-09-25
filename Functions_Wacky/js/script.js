b83a5cfb2ddc86473a8dc591c14b5fe41d741987
Shelby Foegelle <shelbyfoegelle@gmail.com>
2014-09-25 11:36:48 -0500

    Functions Wacky 20 - now compares the price of two pizzas

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,22 +5,8 @@
  */
 
 // Pizza Value Evaluator
-// Takes the size of a pizza and the cost and tells the user the cost per square inch of pizza
+// Takes the size of two pizzas and calculates the cost per square inch for each pizza and tells the user which one has more value
 // Assumes the user is buying a whole round pizza
-
-// Gets the diameter of the pizza from the user and checks that it is the correct input type
-var diameterOfPizza = prompt("What is the diameter of the pizza (in inches)?");
-while(isNaN(diameterOfPizza)) {
-    alert("Incorrect Input!");
-    diameterOfPizza = prompt("What is the diameter of the pizza (in inches)?\nNumbers only please!");
-}
-
-// Gets the price of the pizza from the user and checks that it is the correct input type
-var priceOfPizza = prompt("What is the price of the pizza?");
-while(isNaN(priceOfPizza)) {
-    alert("Incorrect Input!");
-    priceOfPizza = prompt("What is the price of the pizza?\nNumbers only please!");
-}
 
 // Calculates the area of a circle or more importantly the area of a pizza :)
 function areaOfCircle(diameter) {
@@ -33,12 +19,41 @@ function pricePerSquareInch(area, price) {
     return price / area;
 }
 
+// Gets the diameter of pizza 1 from the user and checks that it is the correct input type
+var diameterOfPizza1 = prompt("What is the diameter of the first pizza (in inches)?");
+while(isNaN(diameterOfPizza1)) {
+    alert("Incorrect Input!");
+    diameterOfPizza1 = prompt("What is the diameter of the first pizza (in inches)?\nNumbers only please!");
+}
+
+// Gets the price of pizza 1 from the user and checks that it is the correct input type
+var priceOfPizza1 = prompt("What is the price of the first pizza?");
+while(isNaN(priceOfPizza1)) {
+    alert("Incorrect Input!");
+    priceOfPizza1 = prompt("What is the price of the first pizza?\nNumbers only please!");
+}
+
+// Gets the diameter of pizza 2 from the user and checks that it is the correct input type
+var diameterOfPizza2 = prompt("What is the diameter of the second pizza (in inches)?");
+while(isNaN(diameterOfPizza1)) {
+    alert("Incorrect Input!");
+    diameterOfPizza1 = prompt("What is the diameter of the second pizza (in inches)?\nNumbers only please!");
+}
+
+// Gets the price of pizza 2 from the user and checks that it is the correct input type
+var priceOfPizza2 = prompt("What is the price of the second pizza?");
+while(isNaN(priceOfPizza1)) {
+    alert("Incorrect Input!");
+    priceOfPizza1 = prompt("What is the price of the second pizza?\nNumbers only please!");
+}
+
+
 // Stores the area of the pizza
-var areaOfPizza = areaOfCircle(diameterOfPizza);
+var areaOfPizza1 = areaOfCircle(diameterOfPizza1);
+var areaOfPizza2 = areaOfCircle(diameterOfPizza2);
+// Stores the price of the pizza and sets the number of decimal places to 2
+var priceOfPizzaPerSquareInch1 = (pricePerSquareInch(areaOfPizza1, priceOfPizza1)).toFixed(2);
+var priceOfPizzaPerSquareInch2 = (pricePerSquareInch(areaOfPizza2, priceOfPizza2)).toFixed(2);
 
-// Stores the price of the pizza
-var priceOfPizzaPerSquareInch = pricePerSquareInch(areaOfPizza, priceOfPizza);
+console.log("The price of pizza per square inch is $" + priceOfPizzaPerSquareInch1);
 
-
-console.log("The diameter of the pizza is " + diameterOfPizza);
-console.log("The price of the pizza is " + priceOfPizza);
